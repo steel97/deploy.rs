2956ed3c39492f4f8102e582addecca04b8336b9
Ivan Yv <steel-97@mail.ru>
2023-09-05 18:27:26 +0300

    First deployment step

--- src/deployment/deployment.rs
+++ src/deployment/deployment.rs
@@ -10,6 +10,9 @@ use russh_keys::*;
 use std::{any, collections::HashMap, sync::Arc};
 use tokio::{fs::File, io::BufReader};
 
+use super::packaging::PackageCreator;
+
+static SUDO_PREPEND: &str = "sudo ";
 struct Client {}
 
 #[async_trait]
@@ -73,25 +76,21 @@ pub async fn deploy(
 
     // parse credentials
     let (auth_type, auth_str) = target.authentication.iter().next().unwrap(); // should panic if config has errors
-    let target_package_names: HashMap<String, String> = HashMap::new();
+    let mut target_package_names: HashMap<String, String> = HashMap::new();
+    let mut checksums: HashMap<String, HashMap<String, String>> = HashMap::new();
 
     'preDeployConnection: loop {
         // create ssh session
         let mut session: Handle<Client>;
 
-        loop {
-            // ssh config
-            let ssh_config = russh::client::Config::default();
-            let ssh_config = Arc::new(ssh_config);
-            let sh = Client {};
-            match russh::client::connect(ssh_config, (target.host.to_owned(), target.port), sh)
-                .await
-            {
-                Err(..) => continue,
-                Ok(res) => {
-                    session = res;
-                    break;
-                }
+        // ssh config
+        let ssh_config = russh::client::Config::default();
+        let ssh_config = Arc::new(ssh_config);
+        let sh = Client {};
+        match russh::client::connect(ssh_config, (target.host.to_owned(), target.port), sh).await {
+            Err(..) => continue 'preDeployConnection,
+            Ok(res) => {
+                session = res;
             }
         }
 
@@ -108,30 +107,23 @@ pub async fn deploy(
                 //let key =
                 //    Arc::new(russh_keys::openssh::decode_openssh(reader.buffer(), cert_pass).unwrap());
                 let key = Arc::new(russh_keys::load_secret_key(creds[1], cert_pass).unwrap()); // should panic if key wrong
-                loop {
-                    match session.authenticate_publickey(creds[0], key.clone()).await {
-                        Err(..) => continue,
-                        Ok(res) => {
-                            auth = res;
-                            break;
-                        }
+                match session.authenticate_publickey(creds[0], key.clone()).await {
+                    Err(..) => continue 'preDeployConnection,
+                    Ok(res) => {
+                        auth = res;
                     }
                 }
             }
-            "password" => loop {
-                match session.authenticate_password(creds[0], creds[1]).await {
-                    Err(..) => continue,
-                    Ok(res) => {
-                        auth = res;
-                        break;
-                    }
+            "password" => match session.authenticate_password(creds[0], creds[1]).await {
+                Err(..) => continue 'preDeployConnection,
+                Ok(res) => {
+                    auth = res;
                 }
             },
             _ => {}
         }
         //println!("auth = {}", auth);
         if auth {
-            let mut channel = session.channel_open_session().await?;
             // 3. deploy packages
             for package in &target.packages {
                 // check if we already deployed package
@@ -139,8 +131,9 @@ pub async fn deploy(
                     continue;
                 }
 
+                let mut channel = session.channel_open_session().await?;
                 channel.exec(true, "mktemp").await?; // ignore sudo here (important)
-                let mut tmp_file_name: String;
+                let mut tmp_file_name = String::new();
                 while let Some(res) = channel.wait().await {
                     match res {
                         russh::ChannelMsg::Data { ref data } => {
@@ -149,10 +142,50 @@ pub async fn deploy(
                                     let st = v.to_string();
                                     let dt: Vec<&str> = st.split("\n").collect();
                                     tmp_file_name = Some(dt[0]).unwrap_or("").to_string();
-                                    break;
                                 }
                                 Err(_) => {
-                                    println!("shit happend");
+                                    continue 'preDeployConnection;
+                                }
+                            };
+                        }
+                        _ => {}
+                    }
+                }
+
+                // #USE_REMOTE_CHECKSUM
+                // [1/4] Computing checksum {package}
+                if !checksums.contains_key(package) {
+                    checksums.insert(package.to_string(), HashMap::new());
+                }
+
+                // iterate through external files & try to compute all checksums
+                let config_res = config.lock().await;
+                let package_element = &config_res.packages[package];
+                let mut creator = PackageCreator::new(); //new PackageCreator(Array.Empty<byte>());
+                let mut files: Vec<String> = Vec::new();
+                creator.collect_files_ext(package_element.local_directory.to_string(), &mut files);
+                /*for element in &files {
+                    println!("file {}", element);
+                }*/
+                // #USE_REMOTE_CHECKSUM_ACCUMULATED_HASHER
+                let mut cmdpars = String::new();
+                for file in &files {
+                    cmdpars +=
+                        &format!(" \"{}{}\"", package_element.target_directory, file).to_string();
+                }
+
+                let mut channel = session.channel_open_session().await?;
+                let fmt = format!("{}sha1sum{}", SUDO_PREPEND, cmdpars);
+                channel.exec(true, fmt).await?;
+                let mut cmd_res = String::new();
+                while let Some(res) = channel.wait().await {
+                    match res {
+                        russh::ChannelMsg::Data { ref data } => {
+                            match std::str::from_utf8(data) {
+                                Ok(v) => {
+                                    cmd_res += v;
+                                }
+                                Err(_) => {
                                     continue 'preDeployConnection;
                                 }
                             };
@@ -161,11 +194,39 @@ pub async fn deploy(
                     }
                 }
 
+                let cmd_y_res: Vec<&str> = cmd_res.split("\n").collect();
+                for (i, cmyr) in cmd_y_res.iter().enumerate() {
+                    if cmyr == &"" {
+                        continue;
+                    }
+                    let sum_vec: Vec<&str> = cmyr.split(" ").collect();
+                    let sum = sum_vec[0];
+                    checksums.get_mut(package).unwrap().remove(&files[i]); // check for duplicates
+                    checksums
+                        .get_mut(package)
+                        .unwrap()
+                        .insert(files[i].to_string(), sum.to_string());
+                }
+
+                //for chksum in &checksums {
+                /*for (key, value) in &checksums {
+                    for (k1, v1) in value {
+                        println!("{} = {}", k1, v1);
+                    }
+                }*/
+                target_package_names.insert(package.to_string(), tmp_file_name);
+
                 break 'preDeployConnection;
             }
         } else {
             return Err(anyhow!("Authentication failed"));
         }
+    }
+
+    // 2. prepare & upload packages
+    let mut ongoing_deploy_packages_state: Vec<String> = Vec::new();
+    'ongoinDeployConnection: loop {
+        let mut checksums_deep_copy: HashMap<String, HashMap<String, String>> = HashMap::new();
     }
 
     Ok(())
--- src/deployment/packaging.rs
+++ src/deployment/packaging.rs
@@ -1,1 +1,66 @@
+use std::{
+    fs::{self, metadata},
+    path::{Path, PathBuf},
+};
 
+pub struct PackageCreator {}
+
+impl PackageCreator {
+    pub fn new() -> PackageCreator {
+        PackageCreator {}
+    }
+
+    pub fn collect_files_ext(&mut self, local_dir: String, files: &mut Vec<String>) {
+        let ldir = PathBuf::from(local_dir);
+        self.collect_files(
+            ldir.canonicalize().unwrap().to_str().unwrap().to_string(),
+            "".to_string(),
+            files,
+        );
+    }
+
+    pub fn collect_files(&mut self, local_dir: String, cur_dir: String, files: &mut Vec<String>) {
+        let path_to_current_directory = Path::new(&local_dir).join(cur_dir);
+        let paths: Vec<_> = fs::read_dir(path_to_current_directory)
+            .unwrap()
+            .map(|r| r.unwrap())
+            .collect();
+
+        for path in &paths {
+            let file_path = path.path();
+            let md = metadata(file_path).unwrap();
+            if !md.is_file() {
+                continue;
+            }
+
+            let file_path = path.path();
+            let mut name = file_path
+                .into_os_string()
+                .into_string()
+                .unwrap()
+                .replace(&local_dir, "");
+            if name.starts_with('\\') {
+                name = name[1..].to_string();
+            }
+
+            name = name.replace("\\", "/");
+
+            files.push(name);
+        }
+
+        for path in paths {
+            let file_path = path.path();
+            let md = metadata(file_path).unwrap();
+            if !md.is_dir() {
+                continue;
+            }
+
+            let file_path = path.path();
+            self.collect_files(
+                local_dir.to_string(),
+                file_path.to_str().unwrap().to_string(),
+                files,
+            );
+        }
+    }
+}
